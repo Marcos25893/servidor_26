61e206a0490fed854b05fea8ad2d97cabbd7fea9
Marcos2593 <mromjim783@g.educaand.es>
2025-10-16 00:34:32 +0200

    Añadido las habilidades y botones

--- proyectos/ejercicios/Practica1PHP/ej3.php
+++ proyectos/ejercicios/Practica1PHP/ej3.php
@@ -8,23 +8,26 @@
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    
-    
 
   <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand">Clases</a>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Buscar Clase" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Buscar</button>
-      </form>
-    </div>
+    <form class="container-fluid">
+      <div class="d-flex justify-content-between align-items-center w-100 mt-3">
+        <div>
+          <img src="./ej3img/logo.jpg" alt="logo" height="80">
+        </div>
+        <div>
+          <a href="https://diablo3.blizzard.com/es-es/" class="btn btn-outline-danger me-2" type="button">Iniciar Sesión</a>
+          <a href="https://eu.shop.battle.net/es-es/product/diablo-iii#optLogin=true" class="btn btn-outline-success me-2" type="button">Comprar</a>
+          <a href="https://diablo3.blizzard.com/es-es/" class="btn btn-sm btn-outline-secondary" type="button">Probar Gratis</a>
+        </div>
+      </div>
+    </form>
   </nav>
 
-    <div class="flex-shrink-0 p-3 bg-white">
+  <div class="flex-shrink-0 p-3 bg-dark bg-opacity-50">
 
-        <div class="container text-center">
-            <div class="row">
+    <div class="container text-center">
+        <div class="row">
 
   <?php
 
@@ -34,7 +37,8 @@
           "vida" => 1500,
           "url" => "./ej3img/barbaro.webp",
           "desc" => "Un feroz guerrero que utiliza la fuerza bruta para aplastar a sus enemigos.",
-          "habilidades" => array("Whirlwind", "Leap", "Bash")
+          "habilidades" => array("Whirlwind", "Leap", "Bash"),
+          "imagen" => "./ej3img/habbarbaro.png"
       ),
 
       "jugador2" => array(
@@ -42,7 +46,8 @@
           "vida" => 900,
           "url" => "./ej3img/hechicera.jpeg",
           "desc" => "Controla el poder de los elementos para destruir a sus enemigos.",
-          "habilidades" => array("Fireball", "Ice Bolt", "Teleport")
+          "habilidades" => array("Fireball", "Ice Bolt", "Teleport"),
+          "imagen" => "./ej3img/habhechicera.png"
       ),
 
       "jugador3" => array(
@@ -50,7 +55,8 @@
           "vida" => 1000,
           "url" => "./ej3img/nigromante.webp",
           "desc" => "Invoca muertos vivientes y usa magia oscura para controlar el campo de batalla.",
-          "habilidades" => array("Raise Skeleton", "Bone Spear", "Corpse Explosion")
+          "habilidades" => array("Raise Skeleton", "Bone Spear", "Corpse Explosion"),
+          "imagen" => "./ej3img/habnicro.png"
       ),
 
       "jugador4" => array(
@@ -58,7 +64,8 @@
           "vida" => 1200,
           "url" => "./ej3img/paladin.jpg",
           "desc" => "Guerrero sagrado que usa habilidades defensivas y ofensivas.",
-          "habilidades" => array("Holy Shield", "Zeal", "Smite")
+          "habilidades" => array("Holy Shield", "Zeal", "Smite"),
+          "imagen" => "./ej3img/habpala.png"
       ),
 
       "jugador5" => array(
@@ -66,7 +73,8 @@
           "vida" => 850,
           "url" => "./ej3img/cazador.webp",
           "desc" => "Especialista en ataques a distancia y trampas contra demonios.",
-          "habilidades" => array("Multi-Shot", "Caltrops", "Vault")
+          "habilidades" => array("Multi-Shot", "Caltrops", "Vault"),
+          "imagen" => "./ej3img/habcaza.png"
       ),
 
       "jugador6" => array(
@@ -74,7 +82,8 @@
           "vida" => 1100,
           "url" => "./ej3img/monje.jpeg",
           "desc" => "Guerrero ágil que combina artes marciales y magia espiritual.",
-          "habilidades" => array("Fists of Thunder", "Seven-Sided Strike", "Sweeping Wind")
+          "habilidades" => array("Fists of Thunder", "Seven-Sided Strike", "Sweeping Wind"),
+          "imagen" => "./ej3img/habmonje.png"
       ),
 
       "jugador7" => array(
@@ -82,7 +91,8 @@
           "vida" => 950,
           "url" => "./ej3img/mago.webp",
           "desc" => "Maestro de la magia arcana que usa hechizos devastadores.",
-          "habilidades" => array("Magic Missile", "Arcane Orb", "Teleport")
+          "habilidades" => array("Magic Missile", "Arcane Orb", "Teleport"),
+          "imagen" => "./ej3img/habmago.jpeg"
       )
     );
 
@@ -93,18 +103,26 @@
       echo '<div class="col">';
 
       echo '<div class="card" style="width: 18rem; margin-bottom:30px;">
-        <img src=' . $valor["url"] . ' class="card-img-top" alt="...">
+        <img src=' . $valor["url"] . ' class="card-img-top" alt="..." style="height: 500px">
         <div class="card-body">
           <h5 class="card-title">' . $valor["nombre"] . '</h5>
           <p class="card-text">' . $valor["desc"] . '</p>
         </div>';
-
+/*
+        echo '<h5 class="card-title"> Habilidades </h5>';
         foreach($valor["habilidades"] as $hab){
           echo '<ul class="list-group list-group-flush">
-            <li class="list-group-item">' . $hab . '</li>
+            <li class="list-group-item d-flex align-items-center justify-content-between">' . $hab . ' <img src="./ej3img/habilidad.png" alt="habilidad" height="40" gap="30"></li>
           </ul>';
         }
-
+*/
+        echo '<h5 class="card-title"> Habilidades </h5>';
+        foreach($valor["habilidades"] as $hab){
+          echo '<ul class="list-group list-group-flush">
+            <li class="list-group-item d-flex align-items-center justify-content-between">' . $hab . 
+            '<img src="' . $valor["imagen"] .'" alt="habilidad" height="40"> </li>
+          </ul>';
+        }
       echo '</div>
       </div>';
 
