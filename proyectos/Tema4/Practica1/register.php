0f2a06952c3355db1dc614544787fbf1fc01e5f3
Marcos2593 <mromjim783@g.educaand.es>
2025-11-11 12:53:56 +0100

    Arreglado el head de login y register

--- proyectos/Tema4/Practica1/register.php
+++ proyectos/Tema4/Practica1/register.php
@@ -1,4 +1,20 @@
-<?php include_once('header.php'); ?>
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./css/fontawesome.css">
+    <link href="./assets/css/solid.css" rel="stylesheet" />
+</head>
+<body>
+    
 <body class="bg-primary">
 
 <div  class="d-flex justify-content-center mt-5" style="height: 100vh;">
